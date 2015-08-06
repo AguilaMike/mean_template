68023d74676269d0bdea15ff4c6f35204cac91f6
Mario Cordovilla <mariocordovilla@agorabinaria.com>
2015-08-06 19:16:06 +0200

    Forgot password compontent.

--- src/client/app/_common/app.js
+++ src/client/app/_common/app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 angular
-    .module('template', ['ngResource', 'ngAnimate', 'ui.router', 'navbar', 'login', 'dashboard', 'register'])
+    .module('template', ['ngResource', 'ngAnimate', 'ui.router', 'navbar', 'login', 'dashboard', 'register', 'forgotPassword'])
     .constant('settings', {
         urlBase: 'http://localhost:3030'
     });
