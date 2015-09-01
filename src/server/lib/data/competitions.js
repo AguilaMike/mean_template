4f0f96617a4ac1ca33e971ac3414ad3dbf149011
Alberto Basalo <albertobasalo@hotmail.com>
2015-09-01 14:01:52 +0200

    metrics module fail

--- src/server/lib/data/competitions.js
+++ src/server/lib/data/competitions.js
@@ -1,5 +1,4 @@
 var colName = 'competitions';
-<<<<<<< HEAD
 var _crud = require('./util/crud.js')
     .crud(colName,
         { _id: 1 });
@@ -8,12 +7,3 @@ exports.crud = _crud;
 
 
 
-=======
-var Crud = require('./crud.js');
-var crud = new Crud(colName);
-
-exports.finding = function(mongoQuery){ return crud.finding(mongoQuery,null, null);};
-exports.inserting = function(document){ return crud.inserting(document);};
-exports.updating = function(id,document){ return crud.updating(id,document);};
-exports.removing = function(id){ return crud.removing(id);};
->>>>>>> 227f60fd8b5f30e1a7b0806924ec94c5a6cbb47f
--- src/server/lib/data/riders.js
+++ src/server/lib/data/riders.js
@@ -1,35 +1,7 @@
 var colName = 'riders';
-<<<<<<< HEAD
 var _crud = require('./util/crud.js')
     .crud(colName,
         { total_victories: -1,
         name: 1 });
 
 exports.crud = _crud;
-
-=======
-var Crud = require('./crud.js');
-var crud = new Crud(colName);
-
-exports.finding = function(mongoQuery){ 
-    return crud.finding(mongoQuery,{
-        total_victories: -1,
-        name: 1}
-        ,{ $or: [{
-                _id: mongoQuery.search
-            }, {
-                lob: mongoQuery.search
-            }, {
-                team: mongoQuery.search
-            }, {
-                safe_name: mongoQuery.search
-            }, {
-                team_rol: mongoQuery.search
-                }
-            ]}
-        );
-    };
-exports.inserting = function(document){ return crud.inserting(document);};
-exports.updating = function(id,document){ return crud.updating(id,document);};
-exports.removing = function(id){ return crud.removing(id);};
->>>>>>> 227f60fd8b5f30e1a7b0806924ec94c5a6cbb47f
