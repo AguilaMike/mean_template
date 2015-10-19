67cf0e7b2070ed604bc1b8830c4f922309b3e09b
Alberto Basalo <albertobasalo@hotmail.com>
2015-10-19 18:59:14 +0200

    hashed url

--- src/client/app/_common/states.js
+++ src/client/app/_common/states.js
@@ -9,8 +9,10 @@
         /** used to avoid 404 urls , to be enhanced...  */
         $urlRouterProvider.otherwise('/');
         /** use only if you need pretty (without hash #) url*/
+        /*
         $locationProvider.html5Mode(true);
         $locationProvider.hashPrefix('!');
+        */
     }
     
 })();
