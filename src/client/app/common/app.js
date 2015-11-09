c598fc09a2507fbf170b5bc79c34750b8afed9d2
Alberto Basalo <albertobasalo@hotmail.com>
2015-11-09 19:49:09 +0100

    transactions

--- src/client/app/common/app.js
+++ src/client/app/common/app.js
@@ -3,11 +3,11 @@ var moduleName = "template"
 "use strict";
 
 /** external and own modules */
-var dependencies = ['ui.router', 'ngStorage', 'mainsection', 'navbar', 'footerbar', 'dashboard', 'user','movimientos']
-/** common values to be used across the application */
+var dependencies = ['ui.router', 'ngStorage', 'mainsection', 'navbar', 'footerbar', 'dashboard', 'user', 'transactions']
+	/** common values to be used across the application */
 var settings = {
-    urlBase: 'http://localhost:3030'
+	urlBase: 'http://localhost:3030'
 };
 angular
-    .module(moduleName, dependencies)
-    .constant('settings', settings);
+	.module(moduleName, dependencies)
+	.constant('settings', settings);
